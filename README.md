465ccb9ffd1cd78ba63455bd62080d2219b5f806
Caleb Courtney <brokenfoot@rocketmail.com>
2019-09-16 16:28:43 -0700

    abstracted to allow any number of inputs

--- README.md
+++ README.md
@@ -1,39 +1,52 @@
 # natural_spline
-Finds the natural cubic spline of three points. Can also output the values at regular intervals along the curve, and can find the value on the curve at a given point.
-
-Eventually, this should be expanded to allow for any number of input data points, but using only the cli, I want to stick with just 3.
+Finds the natural cubic spline of a set of input points. Can also output the values at regular intervals along the curve, and can find the value on the curve at a given point.
 
 # Run
 ```
 cd natural_spline
 dub build
-./natural_spline -x0 1 -x1 5 -y0 2 -y1 3
+./natural_spline -x=1,1.2,1.3 -y=10,20,30
 ```
 
 Output:
 ```
-1,2
-1.33333,5.6142
-1.66667,9.08025
-2,12.25
-2.33333,14.9753
-2.66667,17.108
-3,18.5
-3.33333,19.0031
-3.66667,18.4691
-4,16.75
-4.33333,13.6975
-4.66667,9.16358
-5,3
+1,10
+1.01,10.0013
+1.02,10.01
+1.03,10.0337
+1.04,10.08
+1.05,10.1562
+1.06,10.27
+1.07,10.4288
+1.08,10.64
+1.09,10.9113
+1.1,11.25
+1.11,11.6638
+1.12,12.16
+1.13,12.7463
+1.14,13.43
+1.15,14.2188
+1.16,15.12
+1.17,16.1413
+1.18,17.29
+1.19,18.5738
+1.2,20
+1.21,21.63
+1.22,23.44
+1.23,25.31
+1.24,27.12
+1.25,28.75
+1.26,30.08
+1.27,30.99
+1.28,31.36
+1.29,31.07
 ```
 
 You can also run:
 
-`./natural_spline -x0 1 -x1 5 -y0 2 -y1 3`
+`./natural_spline -x=1,2,3,4,5 -y=10,20,30,40,50 -n=4.5`
 
 Output:
 ```
-a -> 2
-b -> 10.9167
-d -> -0.666667
+40
 ```
--- natural_spline/source/app.d
+++ natural_spline/source/app.d
@@ -1,127 +1,166 @@
-/*
+/+
 
-S(n) = a + b(n - x0) + c(n - x0)^2 + d(n - x0)^3
-S(x0) = y0 = a
+####
+Initial calculations that I used to derive the borader, elementwise workflow
 
-S(x1) = y1 = y0 + b(x1 - x0) + c(x1 - x0)^2 + d(x1 - x0)^3
-y1 - y0 = b(x1 - x0) + c(x1 - x0)^2 + d(x1 - x0)^3
-b(x1 - x0) = y1 - y0 - c(x1 - x0)^2 - d(x1 - x0)^3
-b = (y1 - y0 - c(x1 - x0)^2 - d(x1 - x0)^3) / (x1 - x0)
+S0(n) = a0 + b0(n - x0) + c0(n - x0)^2 + d0(n - x0)^3
+S1(n) = a1 + b1(n - x1) + c1(n - x1)^2 + d1(n - x1)^3
+S2(n) = a2 + b2(n - x2) + c2(n - x2)^2 + d2(n - x2)^3
+S3(n) = a3 + b3(n - x3) + c3(n - x3)^2 + d3(n - x3)^3
 
-S'(n) = b + 2c(n - x0) + 3d(n - x0)^2
-S'(x1) = b + 2c(x1 - x0) + 3d(x1 - x0)^2
+S0(x0) = y0 = a0
+S1(x1) = y1 = a1
+S2(x2) = y2 = a2
+S3(x3) = y3 = a3
+S3(x4) = y4 = y3 + b3(x4 - x3) + c3(x4 - x3)^2 + d3(x4 - x3)^3
 
-S''(n) = 2c + 6d(n - x0)
-S''(x0) = 2c = 0
-S''(x1) = 0 = 6d(x1 - x0)
+Boundary conditions:
+S0'(x0) = b0 = 0
+S0'(x1) = S1'(x1)
+S1'(x2) = S2'(x2)
+S2'(x3) = S3'(x3)
+S3'(x4) = 0
 
-c = 0
-d = (x0 - x1) / 6
-b = (y1 - y0 - d(x1 - x0)^3) / (x1 - x0)
+S0''(x0) = 0                -> c0 = 0
+S0''(x1) = S1''(x1) -> 3 * d0(x1 - x0) = 2c1
+S1''(x2) = S2''(x2)
+S3''(x2) = S3''(x3)
+s3''(x4) = 0 = 2c3 + 6d3 (x4 - x3)
 
-S(n) = y0 + b(n - x0) + d(n - x0)^3
+Let's drop the c values from our original equations and substitute back in the y values as well
 
-*/
+S0(n) = y0 + d0(n - x0)^3
+S1(n) = y1 + b1(n - x1) + c1(n - x1)^2 + d1(n - x1)^3
+S2(n) = y2 + b2(n - x2) + c2(n - x2)^2 + d2(n - x2)^3
+S3(n) = y3 + b3(n - x3) + c3(n - x3)^2 + d3(n - x3)^3
+
+S0'(x0) = b0 = 0
+S0'(x1) = 3 * d0(x1 - x0)^2 = b1
+S1'(x2) = b1 + 2c1(x2 - x1) + 3 * d1(x2 - x1)^2 = b2
+S2'(x3) = b2 + 2c1(x2 - x2) + 3 * d2(x3 - x2)^2 = b3
+S3'(x4) = b3 + 2c2(x3 - x3) + 3 * d3(x4 - x3)^2 = 0
+
+S0''(x1) = 6 * d0(x1-x0)
+S1''(x1) = 2c1
+c1 = 3 * d0(x1 - x0)
+
+S1''(x2) = 2c1 + 6 * d1(x2-x1) = c2
+S2''(x3) = 2c2 + 6 * d2(x3-x2) = c3
+S3''(x4) = 2c3 + 6 * d3(x4-x3) = 0 -> d3 = -c3 / (3 * (x4 - x3))
+
+########
+current known values:
+x0, x1, x2, x3, x4 (given)
+y0, y1, y2, y3, y4 (given)
+a0, a1, a2, a3 = y0, y1, y2, y3, y4
+b0, c0 = 0 (by constraints)
+
+S0(x1) = y1 = y0 + d0(x1 - x0)^3
+
+d0 = (y1 - y0) / (x1 - x0)^3
+
+b1 = 3 * d0(x1 - x0)^2
+c1 = 3 * d0(x1 - x0) / 2
+a1 + b1(n - x1) + c1(n - x1)^2 + d1(n - x1)^3
+d1 = (a2 - a1 - b1(x2 - x1) - c1(x2 - x1)^2) / (x2 - x1)^3
+
++/
 
 import std.stdio;
 import std.algorithm;
 import std.math;
 import std.conv;
-
-
-
+import std.getopt;
+import std.string;
 
 
 void main(string[] args)
 {
-    foreach (arg; ["-x0", "-x1", "-y0", "-y1"])
+    string xValsString;
+    string yValsString;
+
+    double[] x;
+    double[] y;
+
+    double findX;
+
+    auto helpInformation = getopt(
+        args,
+        "x", &xValsString,
+        "y", &yValsString,
+        "n", &findX
+    );
+
+    assert(xValsString != "", "`x` values must be given");
+    assert(yValsString != "", "`y` values must be given");
+
+    if (helpInformation.helpWanted)
     {
-        if (!args.canFind(arg))
-        {
-            writeln("missing required argument ", arg);
-            return;
-        }
+        defaultGetoptPrinter("Some basic info", helpInformation.options);
     }
 
-    double x0;
-    try {
-        x0 = to!double(args[countUntil(args, "-x0") + 1]);
-    }
-    catch (std.conv.ConvException)
+    foreach(xVal; xValsString.split(","))
     {
-        writeln("x0 has an incorrect format. try again.");
-        return;
+        x ~= to!double(xVal);
     }
 
-    double x1;    
-    try {
-        x1 = to!double(args[countUntil(args, "-x1") + 1]);
-    }
-    catch (std.conv.ConvException)
+    foreach(yVal; yValsString.split(","))
     {
-        writeln("x1 has an incorrect format. try again.");
-        return;
+        y ~= to!double(yVal);
     }
 
-    double y0;
-    try {
-        y0 = to!double(args[countUntil(args, "-y0") + 1]);
-    }
-    catch (std.conv.ConvException)
+    // make sure we have enough data in the proper format
+    assert(x.length == y.length, "`x` and `y` must have equal number of observations");
+    assert(x.isStrictlyMonotonic, "`x` values must be sorted and monotonic");
+    assert(y.isStrictlyMonotonic, "`y` values must be sorted and monotonic");
+    assert(y.length > 1, "must have more than just one data point observation to create a natural spline");
+    assert((findX > x[0]) && (findX < x[$ - 1]), "`n` value of `" ~ to!string(findX) ~ "` is outside of the input range [" ~ to!string(x[0]) ~ "," ~ to!string(x[$ - 1]) ~ "]");
+
+    double[] a;
+    double[] b;
+    double[] c;
+    double[] d;
+
+    a ~= y[0];
+    b ~= 0;
+    c ~= 0;
+    d ~= (y[1] - y[0]) / pow((x[1] - x[0]), 3);
+
+    for (int counter = 1; counter < x.length - 1; ++counter)
     {
-        writeln("y0 has an incorrect format. try again.");
-        return;
+        a ~= y[counter];
+        b ~= b[counter - 1] + (2 * c[counter - 1] * (x[counter] - x[counter - 1])) + (3 * d[counter - 1] * pow((x[counter] - x[counter - 1]), 2));
+        c ~= (2 * c[counter - 1]) + (6 * d[counter - 1] * (x[counter] - x[counter - 1]));
+        d ~= (y[counter + 1] - a[counter] - (b[counter] * (x[counter + 1] - x[counter])) - (c[counter] * pow((x[counter + 1] - x[counter]), 2))) / pow((x[counter + 1] - x[counter]), 3);
     }
 
-    double y1;
-    try {
-        y1 = to!double(args[countUntil(args, "-y1") + 1]);
-    }
-    catch (std.conv.ConvException)
-    {
-        writeln("y1 has an incorrect format. try again.");
-        return;
-    }
-
-    if (x0 == x1)
-    {
-        writeln("x0 and x1 cannot be the same value for a cubic spline");
-        return;
-    }
-
-    assert(x0 != x1);
-
-    double a = y0;
-    double d = (x0 - x1) / 6.0;
-    double b = (y1 - y0 - d * pow(x1 - x0, 3)) / (x1 - x0);
-
-    double sections;
-
-    // if the user wants us to split out values based on the input
-    if (args.canFind("-n"))
-    {
-        try
-        {
-            sections = to!double(args[countUntil(args, "-n") + 1]);
-        }
-        catch (std.conv.ConvException)
-        {
-            writeln("n has an incorrect format. try again.");
-            return;
-        }
-
-        double interval = (x1 - x0) / sections;
-        for (double n = x0; n <= x1; n += interval)
-        {
-            // y0 + b(n - x0) + d(n - x0)^3
-            writeln(n, ",", y0 + b * (n - x0) + d * pow((n - x0), 3));
-        }
-    }
-
+    if (!findX.isNaN)
+        writeln(calculateOnSpline(x, y, a, b, c, d, findX));
     else
     {
-        writeln("a -> ", a);
-        writeln("b -> ", b);
-        writeln("d -> ", d);        
+        for (double i = x[0]; i < x[$ - 1]; i += 0.01)
+        {
+            writeln(i, ",", calculateOnSpline(x, y, a, b, c, d, i));
+        }
     }
+}
+
+
+double calculateOnSpline(double[] x, double[] y, double[] a, double[] b, double[] c, double[] d, double findX)
+{
+    int xRange = 0;
+    auto prevVal = x[0];
+
+    foreach (val; x[1 .. $])
+    {
+        if (findX <= val && findX >= prevVal)
+            break;
+        else
+        {
+            ++xRange;
+            prevVal = val;
+        }
+    }
+
+    return a[xRange] + b[xRange] * (findX - x[xRange]) + c[xRange] * pow((findX - x[xRange]), 2) + d[xRange] * pow((findX - x[xRange]), 3);
 }
